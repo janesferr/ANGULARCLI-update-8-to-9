fa072b4721cdaa11573a786db88926e47fc88525
Jane Sferrazza <janesferrazza@Janes-Air.localdomain>
2024-07-16 12:17:30 -0400

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCbt5N_Z44pT1hdGf18dD_uwtYnU7RNcE0',
+    authDomain: 'angular-firebase-grid-dash.firebaseapp.com',
+    databaseURL: 'https://angular-firebase-grid-dash.firebaseio.com',
+    projectId: 'angular-firebase-grid-dash',
+    storageBucket: 'angular-firebase-grid-dash.appspot.com',
+    messagingSenderId: '985074974658'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
